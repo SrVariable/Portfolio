a22d72cf41bc7aaab8c00105a7f960bfa2f9fa4a
SrVariable <rojohnibana@gmail.com>
2025-04-19 01:56:20 +0200

    Implement design for mobile

---
+++ src/const/project.ts
@@ -0,0 +1,50 @@
+export const PROJECTS = [
+    {
+        name: 'Mongo exporter - A simple metric exporter',
+        description: 'Metric exporter for a MongoDB database. It collects metrics from a database and exposes them in a format that can be scraped by Prometheus and displayed in Grafana. Developed in Go as part of my intership in iAR Soft.',
+        technologies: [
+            'Go',
+            'Docker',
+            'Prometheus',
+            'Git',
+            'Grafana',
+            'MongoDB',
+        ],
+        image: '/images/mongo-exporter.webp',
+        source: 'https://github.com/SrVariable/mongo-exporter',
+    },
+    {
+        name: 'Cub3D - A simple 2.5D game like DOOM',
+        description: 'Simple 2.5D game like DOOM. Using raycasting techniques. Developed in C from scratch using MLX42 (graphic library) as part of the 42 School curriculum.',
+        technologies: [
+            'C',
+            'MLX42',
+            'Git',
+            'Makefile',
+        ],
+        image: '/images/cub3d.webp',
+        source: 'https://github.com/ribana-b/cub3d',
+    },
+    {
+        name: 'DynSoul - A web page for my fictional company',
+        description: 'Web page for my fictional company. Developed using Astro and TailwindCSS as part of my Web Development Higher Technical Degree.',
+        technologies: [
+            'Astro',
+            'TailwindCSS',
+            'Git',
+        ],
+        image: '/images/dynsoul.webp',
+        source: 'https://github.com/SrVariable/DynSoul',
+        url: 'https://dynsoul.vercel.app/',
+    },
+    {
+        name: 'PascuiBOT - A Discord bot for personal use',
+        description: 'Discord bot for personal use. Initially developed in Python, then migrated to Go.',
+        technologies: [
+            'Python',
+            'Go',
+            'Docker',
+            'Git',
+        ],
+    },
+]
