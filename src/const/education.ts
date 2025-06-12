e212b8775c835ad13b6d67e9bb89ce3af9d8f8ce
SrVariable <rojohnibana@gmail.com>
2025-06-12 18:49:15 +0200

    Add Education section

---
+++ src/const/education.ts
@@ -0,0 +1,14 @@
+export const EDUCATIONS = [
+    {
+        title: "Higher Technician in Web Application Development",
+        source: "IES Salduba",
+        duration: "September 2024 - Present",
+        description: "",
+    },
+    {
+        title: "Software Engineering Program",
+        source: "42 Málaga",
+        duration: "April 2023 - Present",
+        description: "",
+    },
+]
