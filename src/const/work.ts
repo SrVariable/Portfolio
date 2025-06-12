adb6f1d11752a52c2c1c106f8d39ca90214b7b83
SrVariable <rojohnibana@gmail.com>
2025-06-12 15:34:14 +0200

    Add Experience Section

---
+++ src/const/work.ts
@@ -0,0 +1,8 @@
+export const WORKS = [
+    {
+        company: "iAR Soft",
+        role: "Internship",
+        duration: "March 2025 - April 2025",
+        description: "During my internship, I developed a microservice to export metrics from MongoDB through a REST API and visualised the data in Grafana, improving the monitorization of the database usage.",
+    },
+]
